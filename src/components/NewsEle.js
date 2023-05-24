622ab5b2a26e30030fe38fa7b52d939dffa921b0
lakshit1504 <121532634+lakshit1504@users.noreply.github.com>
2023-05-24 11:59:38 +0530

    first deploy

---
+++ src/components/NewsEle.js
@@ -0,0 +1,81 @@
+import React from "react";
+
+export default function NewsEle(props) {
+    return (
+    
+        <div className="my-3">
+          <div className="card">
+              <div
+                style={{
+                    display: "flex",
+                    justifyContent: "flex-end",
+                    position: "absolute",
+                    right: "0",
+                  }}
+                >
+                  <span className="  badge rounded-pill bg-danger ">{props.source}</span>
+                </div>
+                <img src={props.imageUrl} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">{props.title}...</h5>
+                  <p className="card-text">{props.description}...</p>
+                  <p className="card-text">
+              {" "}
+              by-{props.author} on {new Date(props.publishedAt).toGMTString()}
+            </p>
+            <a
+              rel="noreferrer"
+              href={props.newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-warning"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+    </div>
+  )
+}
+// export class NewsEle extends Component {
+//   render() {
+//     let { title, description, imageUrl, newsUrl, author, publishedAt, source } =
+//       this.props;
+//     return (
+//       <div className="my-3">
+//         <div className="card">
+//           <div
+//             style={{
+//               display: "flex",
+//               justifyContent: "flex-end",
+//               position: "absolute",
+//               right: "0",
+//             }}
+//           >
+//             <span className="  badge rounded-pill bg-danger ">{source}</span>
+//           </div>
+//           <img src={imageUrl} className="card-img-top" alt="..." />
+//           <div className="card-body">
+//             <h5 className="card-title">{title}...</h5>
+//             <p className="card-text">{description}...</p>
+//             <p className="card-text">
+//               {" "}
+//               by-{author} on {new Date(publishedAt).toGMTString()}
+//             </p>
+//             <a
+//               rel="noreferrer"
+//               href={newsUrl}
+//               target="_blank"
+//               className="btn btn-sm btn-warning"
+//             >
+//               Read More
+//             </a>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+// export default NewsEle;
+
+
+
